9bfa39205cd0704b8ac512bd18d9f700bafab8a6
Mario Almengor <mario24elias@gmail.com>
2021-11-07 17:32:08 -0600

    agregando IP de usuario

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello World Flask'
+    user_ip = request.remote_addr
+    return 'Hello World Platzi, tu IP es {}'format(user_ip)
