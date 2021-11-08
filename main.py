50f933a58028bd0a19833d3213462b3fa434d9be
Mario Almengor <mario24elias@gmail.com>
2021-11-07 18:49:54 -0600

    agregando el template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
@@ -16,4 +16,4 @@ def index():
 def hello():
     user_ip = request.cookies.get('user_ip')
 
-    return 'Hello World Platzi desde cabecera, tu IP es {}'.format(user_ip)
+    return render_template('hello.html', user_ip=user_ip)
