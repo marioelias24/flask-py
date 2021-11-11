723274f5543d00d3eebfb097217f98d2b685a22a
Mario Almengor <mario24elias@gmail.com>
2021-11-10 21:54:53 -0600

    Agregados errores y bootstrap

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server(error):
+    return render_template('500.html', error=error)
 
 
 @app.route('/')
