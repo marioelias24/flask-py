cb9be7dc11cb6d869835c35a7f5b4705fc6578b2
Mario Almengor <mario24elias@gmail.com>
2021-11-10 22:38:57 -0600

    Uso de wtf y bootstrap

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import Flaskform
+from wtforms.fields import Stringfield, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
+
+
+class LoginForm(Flaskform):
+    username = Stringfield('Nombre de usuario', validaors=[DataRequired()])
+    password = PasswordField('Password', validaors=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 
 @app.errorhandler(404)
@@ -21,17 +33,19 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    LoginForm = LoginForm()
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form
     }
 
     return render_template('hello.html', **context)
